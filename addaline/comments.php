a4333d9a6cf6b35e4e6a2d40f4b42fcaf915819b
ryfarris666 <ryslasf@scholar.yc.edu>
2026-01-25 22:39:58 -0700

    Add files via upload

---
+++ addaline/comments.php
@@ -0,0 +1,14 @@
+<?php
+// this is a single line comment
+# As is this
+?>
+<?php
+/*
+Multi line comment
+which can span over many lines
+I use something simliar in the past programs
+it shouldn't effect the output
+*/
+
+print "Commets do not display in the browser.";
+?>
