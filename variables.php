9cd7841fbc11ff03265b506e87c4e082bebb08d6
ryfarris666 <ryslasf@scholar.yc.edu>
2026-02-01 03:35:03 -0700

    Add files via upload

---
+++ variables.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Variables</title>
+</head>
+<body>
+<?php // Script 2.3 - variables.php
+
+// Personal information
+$first_name ="Reyna";
+$last_name = "Yslas-Farris";
+$street = "300 E. Bramble Dr.";
+$city = "Paulden";
+$state = "AZ";
+$zip = 86334;
+$phone_number = "(555) 593-5085";
+// print my name and number
+print "<p>My name and number:<br>$first_name <br>$last_name <br>Phone numer:<br>$phone_number";
+// Print the address:
+print "<p>The address is:<br>$street <br>$city $state $zip</p>";
+
+?>
+</body>
+</html>
