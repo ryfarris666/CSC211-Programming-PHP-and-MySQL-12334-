376bc848fa10ebed66ac939c1040493427611013
ryfarris666 <ryslasf@scholar.yc.edu>
2026-02-14 09:38:14 -0700

    Add files via upload

--- The if Conditional/handle_reg.php
+++ The if Conditional/handle_reg.php
@@ -13,39 +13,29 @@ email, password, confirm, year, terms, color, submit */
 // Address error management, if you want.
 ini_set('display_errors',0);
 
-if(empty($_POST['email'])){
-	echo"<p style='color: red;'> email</p>";
-} else {
-	$email = $_POST['email'];
-}
-
-if(empty($_POST['password'])){
-	echo"<p style='color: red;'> password</p>";
-} else {
-	$password = $_POST['password'];
-}
-
-if(empty($_POST['confirm password'])){
-	echo"<p style='color: red;'> confirm password</p>";
-} else {
-	$confirm_password = $_POST['confirm password'];
-}
-
-if(empty($_POST['year born'])){
-	echo"<p style='color: red;'> year born</p>";
-} else {
-	$year_born = $_POST['year born'];
-}	
-
-if(empty($_POST['favorite color'])){
-	echo"<p style='color: red;'> favorite color</p>";
-} else {
-	$favorite_color	= $_POST['favorite color'];
-}	
-	
 // Flag variable to track success:
 $okay = true;
 
+if(empty($_POST['email'])){
+	echo"<p style='color: red;'> email</p>";
+	$okay = false;
+}
+if(empty($_POST['password'])){
+	echo"<p style='color: red;'> password</p>";
+	$okay = false;
+}
+if(empty($_POST['confirm password'])){
+	echo"<p style='color: red;'> confirm password</p>";
+	$okay = false;
+}
+if(empty($_POST['year born'])){
+	echo"<p style='color: red;'> year born</p>";
+	$okay = false;
+}
+if(empty($_POST['favorite color'])){
+	echo"<p style='color: red;'> favorite color</p>";
+	$okay = false;
+}	
 // If there were no errors, print a success message:
 if ($okay) {
 	print '<p>You have been successfully registered (but not really).</p>';
