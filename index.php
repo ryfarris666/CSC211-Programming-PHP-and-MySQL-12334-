784913243b2415be1b10df6c84bacdaffddb74e3
ryfarris666 <ryslasf@scholar.yc.edu>
2026-03-03 14:03:53 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php // script 8.4 - index.php
+/* This is the home page for this site.
+It uses templates to create the layout.*/
+
+// include the header:
+include('templates/header.html');
+// leave the php sectipn to display lots of html:
+?>
+
+<h2>Welcome to a JD Salinger Fan Club!</h2>
+<p>no desire to write out all this latin.</p>
+
+<?php // return to php.
+include('templates/footer.html');
+// include the footer.
+?>
