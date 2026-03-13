269a29632cae8c1ec126b3cc3d1e26469c9e598f
ryfarris666 <ryslasf@scholar.yc.edu>
2026-03-12 21:58:15 -0700

    Add files via upload

---
+++ Add_quote/add_quote.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Add A Quotation</title>
+</head>
+<body>
+<?php // Script 11.1 - add_quote.php
+/* This script displays and handles an HTML form. This script takes text input and stores it in a text file. */
+
+// Identify the file to use:
+$file = '../quotes.txt';
+
+// Check form sub:
+if ($_SERVER['REQUEST_METHOD'] == 'POST') { 
+
+	if (!empty($_POST['quote']) && ($_POST['quote'] != 'Enter your quotation here.') ) {
+	
+	if (is_writable($file)) {
+	//confirm file writeable
+			
+			file_put_contents($file, $_POST['quote'] . PHP_EOL, FILE_APPEND); 
+
+			// print a message:
+			print '<p>Your quotation has been stored.</p>';
+		
+		} else { // Could not open the file.
+			print '<p style="color: red;">Your quotation could not be stored due to a system error.</p>';
+		}
+		
+	} else { 
+		print '<p style="color: red;">Please enter a quotation!</p>';
+	}
+	
+} // End of submitted IF.
+
+// Leave PHP and display the form:
+?>
+
+<form action="add_quote.php" method="post">
+	<textarea name="quote" rows="5" cols="30">Enter your quotation here.</textarea><br>
+	<input type="submit" name="submit" value="Add This Quote!">
+</form>
+
+</body>
+</html>
